3901ac06588224c1fa12a7ef56b48ffcd9d4d985
Lngngyn289 <longnguyenduc98921@gmail.com>
2025-07-22 16:43:38 +0700

    xu ly ngoai le

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './common/guards/jwt.guard';
+import { AllExceptionsFilter } from './common/exceptions/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.setGlobalPrefix('api');
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
